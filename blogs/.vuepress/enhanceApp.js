6b6ecc94b4511019a8156739ed633a7696b896a9
huangzibo133 <1335284742@qq,com>
2023-04-06 17:19:47 +0800

    百度统计接口调用失败则不显示访问量

--- blogs/.vuepress/enhanceApp.js
+++ blogs/.vuepress/enhanceApp.js
@@ -9,37 +9,45 @@ export default ({ router, isServer }) => {
     if (typeof _hmt != "undefined") {
       if (to.path && (flag || to.path != from.path)) {
         _hmt.push(["_trackPageview", to.path]);
-        await getPV().then(res => {
-          let pv = {}
-          if (res.error_code) {
-            // Access token 过期了
-          } else {
-            const items = res.data.result.items || []
-            const page = items[0] || [], vis = items[1] || []
-            const n = page.length
-            if(flag) {
-              let total = 0
-              page.forEach((value, index) => {
-                if(value[0].name.indexOf(window.location.origin) > -1) {
-                  total += vis[index][0]
-                }
-              })
-              pv['home'] = total
-              for(let i = 0; i < n; i++) {
-                pv[page[i][0].name] = vis[i][0]
-              }
+        // 失败过一次就不再请求了
+        if(Vue.prototype.showPV === undefined || Vue.prototype.showPV) {
+          await getPV().then(res => {
+            let pv = {}
+            if (res.error_code) {
+              // Access token 过期了
             } else {
-              const pathUrl = window.location.origin + to.path
-              for(let i = 0; i < n; i++) {
-                if(page[i][0].name == pathUrl) {
-                  pv[pathUrl] = vis[i][0]
-                  break
+              const items = res.data.result.items || []
+              const page = items[0] || [], vis = items[1] || []
+              const n = page.length
+              if(flag) {
+                let total = 0
+                page.forEach((value, index) => {
+                  if(value[0].name.indexOf(window.location.origin) > -1) {
+                    total += vis[index][0]
+                  }
+                })
+                pv['home'] = total
+                for(let i = 0; i < n; i++) {
+                  pv[page[i][0].name] = vis[i][0]
+                }
+              } else {
+                const pathUrl = window.location.origin + to.path
+                for(let i = 0; i < n; i++) {
+                  if(page[i][0].name == pathUrl) {
+                    pv[pathUrl] = vis[i][0]
+                    break
+                  }
                 }
               }
             }
-          }
-          Vue.prototype.pv = pv
-        })
+            Vue.prototype.showPV = true
+            Vue.prototype.pv = pv
+          }).catch(err => {
+            console.log(err)
+            Vue.prototype.showPV = false
+          })
+        }
+
       }
     }
     next();
--- blogs/util/api.js
+++ blogs/util/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+
 let access_token = "121.d72fd270c17d5845f93d2694dff02463.YHyps5Y9tvw-UE6jp8SqnN8XxjjW6CycZjeqb9T.usX1Xw"
 
 export const getPV = () => {
